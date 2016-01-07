d74ceef30dc4ec45d04d55021660b93f442704ad
Adam Duncan <adam.duncan@clock.co.uk>
2016-01-07 20:48:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = init
+
+var Emitter = require('events').EventEmitter
+  , Primus = require('primus')
+  , PrimusEmitter = require('primus-emitter')
+  , Socket = Primus.createSocket(
+      { transformer: 'websockets'
+      , parser: 'JSON'
+      , plugin: { emitter: PrimusEmitter }
+      , pathname: '/control'
+      })
+
+function init(callback) {
+  callback(null, 'kinemoticam', Kinemoticam)
+}
+
+function Kinemoticam(automait, logger, config) {
+  Emitter.call(this)
+  this.automait = automait
+  this.logger = logger
+  this.config = config
+  this.client = new Socket(config.connString)
+}
+
+Kinemoticam.prototype = Object.create(Emitter.prototype)
+
+Kinemoticam.prototype.init = function () {
+  startListening.call(this)
+}
+
+function startListening() {
+  this.client.on('motion', function () {
+    this.emit('motion')
+  }.bind(this))
+}
